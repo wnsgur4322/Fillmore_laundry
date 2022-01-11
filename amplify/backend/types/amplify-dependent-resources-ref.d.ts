8b96b68ed47290e070fcf526883c1abc58c4a8df
wnsgur4322 <32208343+wnsgur4322@users.noreply.github.com>
2022-01-10 21:08:07 -0800

    amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,23 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "fillmorelaundryf01e0ea2": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "fillmorelaundry1706b5a0": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "laundry": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
